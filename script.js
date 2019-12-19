037dacbea2a3d7d92557363e84b706e94ba1198c
Vladimir <voldemar9824@gmail.com>
2019-12-19 11:39:17 +0700

    d
    d

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var video;
+var display;
 
+window.onload = function() {
+	video = document.getElementById("video.mp4");
+};
+
+function play() {
+    video.play();
+}
+
+function pause() {
+    video.pause();
+}
+
+function stop() {
+    video.pause();
+    video.currentTime = 0;
+}
